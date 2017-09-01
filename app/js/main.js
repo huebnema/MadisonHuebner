ee3ef4e687e9d36fe075115e1b79d87182051739
Maddie Huebner <maddie.huebner@gmail.com>
2017-08-31 21:18:27 -0500

    Get the animation to end at the end of the sentence, rather than way past it at the end of the line

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,19 @@
 $(document).foundation();
+
+// Reference:  https://www.w3schools.com/js/js_if_else.asp
+function setGreeting() {
+    var greeting;
+    var time = new Date().getHours();
+    if (time < 10) {
+        greeting = "Good morning.";
+    } else if (time < 20) {
+        greeting = "Good afternoon.";
+    } else {
+        greeting = "Good evening.";
+    }
+    document.getElementById("greeting").innerHTML = greeting;
+}
+
+window.onload = function() {
+    setGreeting();
+};
